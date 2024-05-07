01087e17a176b5e7be05d9e407c1849ec1af6a42
estiven42 <notavop@ucentral.edu.co>
2024-05-07 18:39:16 -0500

    se crea repo local

---
+++ src/main/java/co/estiiven42/quiz_producto_alimenticio/QuizProductoAlimenticioApplication.java
@@ -0,0 +1,13 @@
+package co.estiiven42.quiz_producto_alimenticio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizProductoAlimenticioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizProductoAlimenticioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/estiiven42/quiz_producto_alimenticio/QuizProductoAlimenticioApplicationTests.java
@@ -0,0 +1,13 @@
+package co.estiiven42.quiz_producto_alimenticio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizProductoAlimenticioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
